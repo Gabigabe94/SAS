5f756b38a9e273260b234805347609abd0283a36
gabigabe <mhammed.gabigabe@gmail.com>
2025-09-07 16:39:09 +0100

    modifications sur les conditions

--- day1/conditions/defi9.c
+++ day1/conditions/defi9.c
@@ -12,14 +12,14 @@ int main(){
     case 'h': case 'i': case 'g': case 'k': case 'l': case 'm': case 'n':
     case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
     case 'v': case 'w': case 'x': case 'y': case 'z':
-        printf("Ce caractére fait partie des l'alphabet, l'alphabet est minuscule.\n");
+        printf("Ce caractére fait partie des l'alphabet, alphabet est minuscule.\n");
         break;
 
     case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'J':
     case 'H': case 'I': case 'G': case 'K': case 'L': case 'M': case 'N':
     case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
     case 'V': case 'W': case 'X': case 'Y': case 'Z':
-        printf("Ce caractére fait partie des l'alphabet, l'alphabet est majiscule.\n");
+        printf("Ce caractére fait partie des l'alphabet, alphabet est majuscule.\n");
         break;
     default:
         printf("Ce caractére ne fait pas partie des l'alphabet.\n");
